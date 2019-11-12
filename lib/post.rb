435fdd4dfc0aa9fe9758155165b9587334f03b65
Lucy Weltner <lucyiweltner@gmail.com>
2019-11-12 19:55:02 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,5 +1,6 @@
 class Post 
-  attr_accessor :title, :author 
+  attr_accessor :title
+  attr_reader :author 
   def initialize(title)
     @title = title 
   end
@@ -10,6 +11,13 @@ class Post
     Author.post_count += 1 
   end
   
+  def author_name
+    if self.author 
+      return self.author
+    else 
+      return nil 
+    end 
+  end
   
 end
     
