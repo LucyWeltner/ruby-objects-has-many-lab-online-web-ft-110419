0b9ba35f1f5ae12b0cb5f11eaa0d8f97e9bf07f0
Lucy Weltner <lucyiweltner@gmail.com>
2019-11-12 20:02:17 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,8 +1,11 @@
 class Post 
   attr_accessor :title
-  attr_reader :author 
+  attr_reader :author
+  @@all = []
+  
   def initialize(title)
     @title = title 
+    @@all << self
   end
   
   def author=(author1)
